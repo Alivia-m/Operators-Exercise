ad673c07731b51cf284e183c35436dffbab2332d
Alivia-m <aliviagayle@icloud.com>
2021-06-11 13:33:14 -0500

    operations assignment 1 & 2

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Operator.Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4) ;
+            Console.WriteLine($"17/4 is {quotient} remainder {remainder}");
+
+            //Area of Circle
+
+            double pi = 3.14;
+
+            Console.WriteLine("What is the radius of your circle?");
+            int radius = int.Parse(Console.ReadLine());
+            double product = 3.14 * radius;
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {product}");
+
+            
+
+
+
+
+        }
+    }
+}
